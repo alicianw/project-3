f1a1c23281a879349d7df432c0690936b016ea42
alicia <alicianyambu@gmail.com>
2019-11-04 10:17:04 +0300

    added portfolio pictures

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,23 @@
-$("#zero").click(function() {
-    $("#one h2 p").toggle();
-    $
+$(document).ready(function() {
+$("#one").click(function() {
+
+    $("#one p").toggle();
+    $("#one img").toggle();
+
+});
+
+$("#two").click(function(){
+    $("#two p").toggle();
+    $("#two img").toggle();
+});
+
+$("#three").click(function(){
+
+    $("#three p").toggle();
+    $("#three img").toggle();
+});
+
+
+
+
 });
